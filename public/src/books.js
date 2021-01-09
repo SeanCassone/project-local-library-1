aa9176fb795ca4776d8e7c2bcfac7b87f6955681
Aaron Young <hi@aaronyoung.dev>
2021-01-09 09:47:37 -0500

    refactor: Update book.js variable names

--- public/src/books.js
+++ public/src/books.js
@@ -1,39 +1,31 @@
-function findAuthorById(authors, authorId) {
-  return authors.find((author) => author.id === authorId);
+function findAuthorById(authorsArr, authorIdInt) {
+  return authorsArr.find(({ id: idInt }) => idInt === authorIdInt);
 }
 
-function findBookById(books, bookId) {
-  return books.find((book) => book.id === bookId);
+function findBookById(booksArr, bookIdStr) {
+  return booksArr.find(({ id: idStr }) => idStr === bookIdStr);
 }
 
-function findAllBorrowedBooks(books) {
-  // return an array of books that have been borrowed
-  return books.filter((book) => !book.borrows[0].returned);
-}
-
-function findAllReturnedBooks(books) {
-  // return an array of books that have been returned
-  return books.filter((book) => book.borrows[0].returned);
-}
-
-function partitionBooksByBorrowedStatus(books) {
+function partitionBooksByBorrowedStatus(booksArr) {
   // 1. return an array containing two sub arrays created by filtering books
   //    the first array being borrowed books and the second being returned
   //    books.
-  return [findAllBorrowedBooks(books), findAllReturnedBooks(books)];
+  return [findAllBorrowedBooks(booksArr), findAllReturnedBooks(booksArr)];
 }
 
-function findAccountById(accounts, accountId) {
-  // return the account object that matches the account id
-  return accounts.find((account) => account.id === accountId);
-}
-
-function getBorrowersForBook(book, accounts) {
-  // 1. reduce the book borrows array into an array of borrowers with a returned property
+function getBorrowersForBook(bookObj, accountsArr) {
+  // 1. reduce the book borrows array into an array of borrowers with a returned
+  //    property
   return (
-    book.borrows
-      .reduce((accumulator, { id, returned }) => {
-        return [...accumulator, { ...findAccountById(accounts, id), returned }];
+    bookObj.borrows
+      .reduce((accumulatorArr, { id: idStr, returned: returnedBool }) => {
+        // find the account obj, make a copy swo we don't  mutate the original
+        // account object
+        accountWithReturned = {
+          ...findAccountById(accountsArr, idStr),
+          returned: returnedBool,
+        };
+        return accumulatorArr.concat(accountWithReturned);
       }, [])
       // 2. return the first 10 elements of the array
       .slice(0, 10)
@@ -46,3 +38,22 @@ module.exports = {
   partitionBooksByBorrowedStatus,
   getBorrowersForBook,
 };
+
+function findAllReturnedBooks(booksArr) {
+  // return an array of books that have been returned
+  return booksArr.filter((bookObj) => isBookReturned(bookObj));
+}
+
+function findAllBorrowedBooks(booksArr) {
+  // return an array of books that have been borrowed
+  return booksArr.filter((bookObj) => !isBookReturned(bookObj));
+}
+
+function isBookReturned({ borrows: borrowsArr }) {
+  return borrowsArr[0].returned;
+}
+
+function findAccountById(accountsArr, accountIdStr) {
+  // return the account object that matches the account id
+  return accountsArr.find(({ id: idStr }) => idStr === accountIdStr);
+}
