f47ec33800a97e9d3ba44fec1c00bf2080cff9ec
Aaron Young <hi@aaronyoung.dev>
2021-02-01 13:02:42 -0500

    refactor: Clean up accounts.js and add JSDoc

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,41 +1,79 @@
+const { findAuthorByID } = require("./accountsHelpers/findAuthorByID");
+const {
+  countBorrowsByAccountId,
+} = require("./accountsHelpers/countBorrowsByAccountId");
+const {
+  getArrOfBooksCheckedOutByAccountId,
+} = require("./accountsHelpers/getArrOfBooksCheckedOutByAccountId");
+
+/**
+ * finds the object with the id
+ * @param {Object[]} accountArr an array of account objects represents a person
+ * @param {string} idStr id of account to find
+ * @returns an _object_ representing a person
+ */
 function findAccountById(accountArr, idStr) {
   // return all accounts that match the given id
   return accountArr.find((accountObj) => accountObj.id === idStr);
 }
 
+/**
+ * it returns a sorted array of objects. The objects are sorted alphabetically
+ *  by last name.
+ * @param {Object[]} accountArr an array of accounts
+ * @returns an _array_ of generic _objects_ representing accounts
+ */
 function sortAccountsByLastName(accountArr) {
   return accountArr.sort(
-    ({ name: { last: lastName1Str } }, { name: { last: lastName2Str } }) => {
-      return lastName1Str > lastName2Str ? 1 : -1;
-    }
+    ({ name: { last: lastName1Str } }, { name: { last: lastName2Str } }) =>
+      lastName1Str > lastName2Str ? 1 : -1
   );
 }
 
+/**
+ * finds number of times the account's ID has appeared in a book's borrow array
+ * @param {Object} account an account object
+ * @param {string} account.id the account's id
+ * @param {Object[]} bookArr an array of book objects
+ * @param {Object[]} bookArr[].borrows objects representing a borrow
+ * @returns the _number_ of times the account's ID has appeared in bookArr
+ * borrow array
+ */
 function numberOfBorrows({ id: accountIdStr }, bookArr) {
   // 1. count how many times individual books was borrowed and return the number
   // as the answer,
-  return bookArr.reduce((numberOfBorrowsInt, { borrows: borrowArr }) => {
-    // 2. for each book, add number of borrows to count of borrows for that book
-    //    and return it
-    return (
-      numberOfBorrowsInt + countBorrowsByAccountId(borrowArr, accountIdStr)
-    );
-  }, 0);
+  return bookArr.reduce(
+    (numberOfBorrowsInt, { borrows: borrowArr }) =>
+      // 2. for each book, add number of borrows to count of borrows for that book
+      //    and return it
+      numberOfBorrowsInt + countBorrowsByAccountId(borrowArr, accountIdStr),
+    0
+  );
 }
 
+/**
+ * combines books and authors
+ * @param {Object} param0 an account
+ * @param {Object[]} bookArr objects representing books
+ * @param {Object[]} authorArr objects representing arrays
+ * @returns An _array_ of book-like _objects_ that includes an author
+ * object
+ */
 function booksInPossession({ id: accountIdStr }, bookArr, authorArr) {
-  // 1. get accountId from the account object
-  // 2. filter the list of books for one presently checked out by accountId
+  // filter the list of books for one presently checked out by accountId
   return (
     getArrOfBooksCheckedOutByAccountId(bookArr, accountIdStr)
-      // 3. map the filtered books to create a new object containing all the
-      //    properties of books and adding an author key pointing to author
-      //    object that we'll find using a helper function.
-      //    note that since I am using an arrow function to implicitly return
-      //    an object, I needed to wrap that object in parentheses
-      .map((book) => ({
-        ...book,
-        author: findAuthorByID(book.authorId, authorArr),
+      /**
+       * 3.
+       * map the filtered books to create a new object containing all the
+       * properties of books and adding an author key pointing to author object
+       * that we'll find using a helper function. note that since I am using an
+       * arrow function to implicitly return an object, I needed to wrap that
+       * object in parentheses
+       */
+      .map((bookObj) => ({
+        ...bookObj,
+        author: findAuthorByID(bookObj.authorId, authorArr),
       }))
   );
 }
@@ -46,37 +84,3 @@ module.exports = {
   numberOfBorrows,
   booksInPossession,
 };
-
-// Below are all helper functions.
-
-function findAuthorByID(authorIdInt, authorArr) {
-  // return the author object where id is equal to authorId
-  return authorArr.find((authorObj) => authorObj.id === authorIdInt);
-}
-
-function getArrOfBooksCheckedOutByAccountId(bookArr, accountIdStr) {
-  return bookArr.filter((bookObj) => {
-    // we can do a 'some' on the borrows array to see if its borrowed and
-    // unreturned
-    // copiesAreBorrowedByAccountId is a Higher Order function that returns
-    // another function.
-    return bookObj.borrows.some(copiesAreBorrowedByAccountId(accountIdStr));
-  });
-}
-
-function copiesAreBorrowedByAccountId(accountIdStr) {
-  // this is a higher order function that returns a function intended to be used
-  // with the array method, some.
-  return ({ id: borrowIdStr, returned: returnedBool }) => {
-    // return true if account id matches and its not returned.
-    return borrowIdStr === accountIdStr && !returnedBool;
-  };
-}
-
-function countBorrowsByAccountId(borrowArr, accountIdStr) {
-  // 1. count each book's borrow by account id, there may be more than 1 borrow
-  //    by a given accountId
-  return borrowArr.filter((borrowObj) => {
-    return borrowObj.id === accountIdStr;
-  }).length;
-}
