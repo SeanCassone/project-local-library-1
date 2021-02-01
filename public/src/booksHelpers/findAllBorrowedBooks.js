83171082f4d12fbf020d464d38152a77ae6ea80d
Aaron Young <hi@aaronyoung.dev>
2021-02-01 13:51:49 -0500

    docs: Document the book functions & helpers

--- public/src/booksHelpers/findAllBorrowedBooks.js
+++ public/src/booksHelpers/findAllBorrowedBooks.js
@@ -1,7 +1,11 @@
 const { isBookReturned } = require("./isBookReturned");
 
+/**
+ * finds all books that are checked out
+ * @param {Object[]} booksArr array of objects that represents a book
+ * @returns {Object[]} an array of books that are presently borrowed
+ */
 function findAllBorrowedBooks(booksArr) {
-  // return an array of books that have been borrowed
   return booksArr.filter((bookObj) => !isBookReturned(bookObj));
 }
 module.exports = { findAllBorrowedBooks };
--- public/src/booksHelpers/findAllReturnedBooks.js
+++ public/src/booksHelpers/findAllReturnedBooks.js
@@ -1,7 +1,12 @@
 const { isBookReturned } = require("./isBookReturned");
 
+/**
+ * find all books that have been returned
+ * @param {Object[]} booksArr array of objects that represents a book
+ * @returns {Object[]} an array of objects representing books that have been
+ * returned
+ */
 function findAllReturnedBooks(booksArr) {
-  // return an array of books that have been returned
   return booksArr.filter((bookObj) => isBookReturned(bookObj));
 }
 module.exports = { findAllReturnedBooks };
